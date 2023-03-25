0277837153c1644b3b93fc26ce46b0812751cb59
MITSUTANI, Kazuya <Kazuya.Mitsutani@gmail.com>
2023-03-25 23:43:19 +0900

    escape slash in regexp

--- surfingkeys.config.js
+++ surfingkeys.config.js
@@ -29,7 +29,7 @@ api.unmap('*', /mail.google.com/) // select all (* + a)
 
 // Google Colaboratory and Jupyter notebook
 api.unmapAllExcept([], /colab.research.google.com/);
-api.unmapAllExcept([], /notebooks/ipnyb//);
+api.unmapAllExcept([], /notebooks\/ipnyb//);
 
 
 // Netflix
